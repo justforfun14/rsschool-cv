074369843951bd9f3efbac8c55cfd36f3cc81ed9
Ivan Gusak <ivan.gusak10@gmail.com>
2021-09-15 23:05:51 +0300

    fix: burger menu

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const ary = document.querySelectorAll(".menu-item");
-const button = document.querySelector("#menu-toggle");
 
 ary.forEach((a) =>
     a.addEventListener("click", (e) => {
-        button.checked = !button.checked;
+        menubox.classList.add("transparent");
+        menubox.classList.add("unvisible");
     })
 );
+
+const button_burger = document.querySelector(".button-burger");
+const menubox = document.querySelector(".menubox");
+
+button_burger.addEventListener("click", (e) => {
+    menubox.classList.toggle("transparent");
+    menubox.classList.toggle("unvisible");
+});
